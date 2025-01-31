8bdb486ea993cf7fbc60aeed6851bea2b25e6054
novenary <novenary@kwak.zip>
2025-01-31 23:50:36 +0200

    ADHDinos: allow running the script without auth

--- series/Ryan Keats/ADHDinos/adhdinos.py
+++ series/Ryan Keats/ADHDinos/adhdinos.py
@@ -6,27 +6,28 @@ import json
 import requests
 import requests.auth
 
-reddit_user = os.environ["REDDIT_USER"]
-reddit_password = os.environ["REDDIT_PASSWD"]
-reddit_app_id = os.environ["REDDIT_ID"]
-reddit_app_secret = os.environ["REDDIT_SECRET"]
+reddit_user = os.getenv("REDDIT_USER")
+reddit_password = os.getenv("REDDIT_PASSWD")
+reddit_app_id = os.getenv("REDDIT_ID")
+reddit_app_secret = os.getenv("REDDIT_SECRET")
 
 headers = {
     "User-Agent": "catgirl-v:cubari:v.0.0.69 (by the cg-v gang)",
 }
 
-response = requests.post(
-    "https://www.reddit.com/api/v1/access_token",
-    auth=requests.auth.HTTPBasicAuth(reddit_app_id, reddit_app_secret),
-    data={
-        "grant_type": "password",
-        "username": reddit_user,
-        "password": reddit_password,
-    },
-    headers=headers,
-).json()
-token = response["access_token"]
-headers["Authorization"] = f"bearer {token}"
+if reddit_user is not None:
+    response = requests.post(
+        "https://www.reddit.com/api/v1/access_token",
+        auth=requests.auth.HTTPBasicAuth(reddit_app_id, reddit_app_secret),
+        data={
+            "grant_type": "password",
+            "username": reddit_user,
+            "password": reddit_password,
+        },
+        headers=headers,
+    ).json()
+    token = response["access_token"]
+    headers["Authorization"] = f"bearer {token}"
 
 url = "https://oauth.reddit.com/r/ADHDinos/new.json"
 
